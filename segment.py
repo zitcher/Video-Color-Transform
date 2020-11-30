1701653cf724e78994daff39134d3948cf9b1f60
Zachary Hoffman <hoffking@gmail.com>
2020-11-30 00:10:01 -0500

    segmentation

---
+++ segment.py
@@ -0,0 +1,29 @@
+import torchvision
+from PIL import Image
+import torch
+import numpy as np
+
+
+def segment(image):
+    with torch.no_grad():
+        image = image/255
+        im = torch.tensor(image).unsqueeze(0).permute(0, 3, 1, 2).float()
+        model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+        model = model.eval()
+        out = model(im)[0]
+
+        combined_mask = np.zeros((image.shape[0], image.shape[1]))
+
+        for i in range(len(out['masks'])):
+            mask = out['masks'].squeeze(1)[i].numpy()
+            combined_mask = np.maximum(combined_mask, mask)
+        return (combined_mask * 255).astype(np.uint8)
+
+if __name__ == '__main__':
+    im = np.array(Image.open("./data/luke.jpg"))
+    print(im.shape)
+    seg = segment(im)
+    print(seg.shape)
+
+    seg = Image.fromarray(seg)
+    seg.save('./data/out.png')
