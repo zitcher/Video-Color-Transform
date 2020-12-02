cf985a4cea776e811b592de229b7bd7335bc0d04
Zachary Hoffman <hoffking@gmail.com>
2020-12-02 00:57:28 -0500

    k mediods

--- cluster.py
+++ cluster.py
@@ -1,9 +1,40 @@
 import sklearn
+from segment import load_video
+import numpy as np
+from scipy import spatial
+from tqdm import trange
 
-def find_video_kmediods(video_frames):
+def find_video_mediod(video_frames):
+    data = []
+    for frame in video_frames:
+        counts, options = np.histogram(frame, bins=np.arange(256))
+        data.append(counts)
 
+    data = np.array(data)
+    pairdist = spatial.distance.cdist(data, data)
 
-def load_video(video_path):
+    best = np.argmin(np.sum(pairdist, axis=0))
+    return best
+
+'''
+The paper states:
+In practice, we run the K-medoids algorithm on the frames of the model video 
+to produce one representative model frame for every 30 frames of the model 
+video, i.e., one for every second of a model video sampled at 30fps.
+
+I believe what this means is that we should break the video into 30 frame segments
+and find a mediod for each segment.
+'''
+def find_video_kmediod(frames):
+    representatives = []
+    window_sz = 30
+    for i in trange(0, len(frames), window_sz):
+        sub_section = frames[i:i+window_sz]
+        representatives.append(i + find_video_mediod(sub_section))
+        
+    return representatives
 
 if __name__ == '__main__':
     vp = './data/all_results/src_models/amelie.mp4'
+    frames = load_video(vp)
+    print(find_video_kmediod(frames))
--- segment.py
+++ segment.py
@@ -6,6 +6,20 @@ import cv2
 from tqdm import tqdm, trange
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
 
 def segment(images):
     with torch.no_grad():
@@ -20,10 +34,14 @@ def segment(images):
 
         masks = []
 
+        looking_for = [COCO_INSTANCE_CATEGORY_NAMES.index('person')]
+
         for i in range(len(outputs)):
             out = outputs[i]
             combined_mask = np.zeros((images[i].shape[0], images[i].shape[1]))
             for i in range(len(out['masks'])):
+                if out['labels'][i].item() not in looking_for:
+                    continue
                 mask = out['masks'].squeeze(1)[i].cpu().numpy()
                 combined_mask = np.maximum(combined_mask, mask)
 
