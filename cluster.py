6fa1c38a9f39470b5eb540e620ef6120de523c0e
Zachary Hoffman <hoffking@gmail.com>
2020-12-02 00:04:47 -0500

    frames

---
+++ cluster.py
@@ -0,0 +1,9 @@
+import sklearn
+
+def find_video_kmediods(video_frames):
+
+
+def load_video(video_path):
+
+if __name__ == '__main__':
+    vp = './data/all_results/src_models/amelie.mp4'
--- segment.py
+++ segment.py
@@ -2,31 +2,78 @@ import torchvision
 from PIL import Image
 import torch
 import numpy as np
-import cv2 as cv
+import cv2
+from tqdm import tqdm, trange
 
-def segment(image):
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def segment(images):
     with torch.no_grad():
-        image = image/255
-        im = torch.tensor(image).unsqueeze(0).permute(0, 3, 1, 2).float()
-        model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+        normalized = []
+        for image in images:
+            image = image/255
+            normalized.append(torch.tensor(image).permute(2, 0, 1).float().to(device))
+
+        model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True).to(device)
         model = model.eval()
-        out = model(im)[0]
+        outputs = model(normalized)
 
-        combined_mask = np.zeros((image.shape[0], image.shape[1]))
+        masks = []
 
-        for i in range(len(out['masks'])):
-            mask = out['masks'].squeeze(1)[i].numpy()
-            combined_mask = np.maximum(combined_mask, mask)
+        for i in range(len(outputs)):
+            out = outputs[i]
+            combined_mask = np.zeros((images[i].shape[0], images[i].shape[1]))
+            for i in range(len(out['masks'])):
+                mask = out['masks'].squeeze(1)[i].cpu().numpy()
+                combined_mask = np.maximum(combined_mask, mask)
 
-        combined_mask = 255*(combined_mask - np.min(combined_mask)) / np.max(combined_mask)
+            combined_mask = 255*(combined_mask - np.min(combined_mask)) / np.max(combined_mask)
+            masks.append(combined_mask.astype(np.uint8))
 
-        return combined_mask.astype(np.uint8)
+        return masks 
+
+def load_video(video_path):
+    frames = []
+    cap = cv2.VideoCapture(video_path)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if not ret:
+            cap.release()
+            cv2.destroyAllWindows()
+            return frames
+        frames.append(frame)
+
+def segment_video(frames):
+    out_frames = []
+    window_sz = 8
+    for i in trange(0, len(frames), window_sz):
+        sub_section = frames[i:i+window_sz]
+        out_frames = out_frames + segment(sub_section)
+        
+    return frames
 
 if __name__ == '__main__':
-    im = np.array(Image.open("./data/abby.jpg"))
-    print(im.shape)
-    seg = segment(im)
-    print(seg.shape)
+    # im = np.array(Image.open("./data/abby.jpg"))
+    # im2 = np.array(Image.open("./data/horseback.jpg"))
+    # print(im.shape)
+    # segs = segment([im, im2])
 
-    seg = Image.fromarray(seg)
-    seg.save('./data/out.png')
+    # for i in range(len(segs)):
+    #     seg = segs[i]
+    #     print(seg.shape)
+    #     seg = Image.fromarray(seg)
+    #     seg.save('./data/seg_{}.png'.format(i))
+
+    frames = load_video('./data/all_results/src_models/amelie.mp4')
+    print(frames[0].shape)
+    segs = segment_video(frames)
+
+    fps = 30
+    video_filename = './data/out.mp4'
+    out = cv2.VideoWriter(video_filename, -1, fps, (frames[0].shape[0], frames[0].shape[1]))
+    for i in range(len(segs)):
+        print(segs[i].shape)
+        out.write(segs[i])
+
+    
