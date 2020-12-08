64e6ea31e894de41296db0147e22978994edf4e2
BlooT7 <34358537+BlooT7@users.noreply.github.com>
2020-12-08 00:48:29 -0500

    DONE: Sample video generation

--- cluster.py
+++ cluster.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy import spatial
 from tqdm import trange
 import cv2
-from part2 import get_frame_feature, get_band_feature, chrominance_transfer
+from part2 import get_frame_feature, get_band_feature, chrominance_transfer, get_distance
 from scipy.linalg import fractional_matrix_power, sqrtm
 
 '''
@@ -51,10 +51,11 @@ def dist(stats1, stats2):
         md = np.linalg.norm(mean1 - mean2)**2
 
         dist += tr + md
-
+    
     return dist
 
 def get_stats(frame):
+    
     feats, slices, masks = get_frame_feature(frame)
 
     stack = []
@@ -128,19 +129,65 @@ def video_kmediod(lab_frames):
 
     return medoids_over_30
 
+def naive_transfer(vp, target):
+    target_frames = load_video(target)
+    source_frames = load_video(vp)
+    target_frames, source_frames = [cv2.cvtColor(frame, cv2.COLOR_RGB2Lab) / 255. for frame in target_frames], [cv2.cvtColor(frame, cv2.COLOR_RGB2Lab) / 255. for frame in source_frames]
+    source_feats = [(np.mean(frame, axis=(0,1)), np.std(frame, axis=(0,1))) for frame in source_frames]
+    target_feats = [(np.mean(frame, axis=(0,1)), np.std(frame, axis=(0,1))) for frame in target_frames]
+    output = []
+    for i, frame in enumerate(source_frames):
+        i_target = i / (len(source_frames) - 1) * (len(target_frames) - 1)
+        i_target = min(int(i_target), len(target_frames) - 1)
+        mu_source, std_source = source_feats[i]
+        mu_target, std_target = target_feats[i_target]
+        out_frame = frame.copy()
+        out_frame[:,:,1:] = (out_frame[:,:,1:] - mu_source[1:]) / std_source[1:]
+        out_frame[:,:,1:] = out_frame[:,:,1:] * std_target[1:] + mu_target[1:]
+        out_frame[:,:,1:] = np.clip(out_frame[:,:,1:], 0, 1)
+        out_frame *= 255.
+        out_frame = out_frame.astype(np.uint8)
+        output.append(out_frame)
+    return output
+
 if __name__ == '__main__':
-    vp = 'lucie.mp4'
-    target = 'car.mp4'
+    vp = 'source.mp4'
+    target = 'target.mp4'
+    
+    output_naive = naive_transfer(vp, target)
+    for frame in output_naive:
+        frame = cv2.cvtColor(frame, cv2.COLOR_Lab2RGB)
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(100)
+    result = None
+    size = output_naive[0].shape[:2]
+    size = size[::-1]
+    result = cv2.VideoWriter('naive.mp4', cv2.VideoWriter_fourcc('m','p','4','v'), 30, size)
+    for frame in output_naive:
+        frame = cv2.cvtColor(frame, cv2.COLOR_Lab2RGB)
+        result.write(frame)
+    result.release()
     
     output = video_transfer(vp, target)
     for frame in output:
         frame = cv2.cvtColor(frame, cv2.COLOR_Lab2RGB)
         cv2.imshow('Frame', frame)
         cv2.waitKey(100)
+    result = None
+    size = output[0].shape[:2]
+    size = size[::-1]
+    result = cv2.VideoWriter('result.mp4', cv2.VideoWriter_fourcc('m','p','4','v'), 30, size)
+    for frame in output:
+        frame = cv2.cvtColor(frame, cv2.COLOR_Lab2RGB)
+        result.write(frame)
+    result.release()
+    
+    diff = np.stack([np.sum((frame1-frame2)**2, axis=None) for frame1, frame2 in zip(output_naive, output)])
+    im1, im2 = output_naive[np.argmax(diff)], output[np.argmax(diff)]
+    im1, im2 = cv2.cvtColor(im1, cv2.COLOR_Lab2BGR), cv2.cvtColor(im2, cv2.COLOR_Lab2BGR)
+    
     
     #print(video_kmediod(lab_frame_stats))
-    
-    
 
     # frames = load_video(vp)
 
